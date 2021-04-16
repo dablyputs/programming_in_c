cf2b8bd610eb9443fd1fb1d510021917b94b5b93
david sewell <dsewell@pop-os.localdomain>
2021-04-16 17:06:13 -0400

    Completed exercise 4.

--- 05_making_decisions/ex04_calculator.c
+++ 05_making_decisions/ex04_calculator.c
@@ -11,31 +11,50 @@ int main(void)
     bool quit = 0;
 
     printf("Begin Calculations\n");
-    while (quit == 0){
+    while (quit == 0)
+    {
         printf("+ - * / S E\n");
         scanf("%f %c", &number, &operator);
         switch (operator)
         {
-        case 'S' :
+        case 'S':
             accumulator = number;
             printf("%.5f\n", accumulator);
-            number = 0;
-            operator = '0';
             break;
-        case '+' :
+        case '+':
             printf("%.5f + %.5f = %.5f\n", number, accumulator, (number + accumulator));
             accumulator = (number + accumulator);
-            number = 0;
-            operator = '0';
             break;
-        case 'E' :
+        case '-':
+            printf("%.5f - %.5f = %.5f\n", accumulator, number, (accumulator - number));
+            accumulator = (accumulator - number);
+            break;
+        case '*':
+            printf("%.5f * %.5f = %.5f\n", accumulator, number, (accumulator * number));
+            accumulator = (number * accumulator);
+            break;
+        case '/':
+            if (number == 0)
+            {
+                printf("Division by zero\n");
+            }
+            else
+            {
+                printf("%.5f / %.5f = %.5f\n", accumulator, number, (accumulator / number));
+                accumulator = (accumulator / number);
+            }
+            break;
+        case 'E':
             quit = 1;
             break;
-        
+
         default:
             quit = 1;
             break;
         }
+
+        number = 0;
+        operator= '0';
     }
 
     return 0;
