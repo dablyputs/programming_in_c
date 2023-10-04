b2099598a55031899d8e30d385a9d81136a85c82
dablyputs <david.sewell@gmail.com>
2023-10-04 12:45:19 -0400

    finished exercise 5.

--- 10_pointers/ex4_remove_entry.c
+++ 10_pointers/ex4_remove_entry.c
@@ -1,5 +1,14 @@
+/* 
+Write a function called removeEntry() to remove an entry from a linked list. 
+The sole argument to the procedure should be a pointer to the list. 
+Have the function remove the entry after the one pointed to by the argument. 
+(Why can’t you remove the entry pointed to by the argument?) 
+You need to use the special structure you set up in exercise 3 to handle the 
+special case of removing the first element from the list.
+*/
 #include <stdio.h>
 
+// A signly linked list
 struct entry 
 {
     int value;
@@ -11,13 +20,9 @@ void printList(struct entry *);
 
 void removeEntry(struct entry *head)
 {
-    if (head->next != NULL)
+    if (head->next != NULL) // if the next node isn't null
     {
-        head->next = head->next->next;
-    }
-    else 
-    {
-        head->next = NULL;
+        head->next = head->next->next; // the next node becomes the following node
     } 
 }
 
@@ -25,9 +30,9 @@ void printList(struct entry *list)
 {
     for (int i = 1; list != NULL; ++i)
     {
-        printf("\n%i (%p)\n", i, list);
-        printf(" value: %i\n", list->value);
-        printf(" next: %p\n", list->next);
+        printf("\n%i (%p)\n", i, list); // print the value of i and the address of list
+        printf(" value: %i\n", list->value); // print the value stored in list
+        printf(" next: %p\n", list->next); // print the address of the next node
         list = list->next;
     }
 }
--- 10_pointers/ex5_double_linked_list.c
+++ 10_pointers/ex5_double_linked_list.c
@@ -1,5 +1,5 @@
 /*
-    Define a doubly linked list entry and then write a small program that implements 
+    Define a doubly linked list and then write a small program that implements 
     a small doubly linked list and prints out the elements of the list.
 */
 
@@ -14,11 +14,17 @@ struct entry
 
 // Prototype definitions
 void printlist(struct entry *);
-void insertentry(struct entry *, struct entry *);
 
 void printlist(struct entry *list)
 {
-
+    for (int i = 1; list != NULL; i++)
+    {
+        printf("%i: (%p)\n", i, list);
+        printf("Value: %i\n", list->value);
+        printf("Next Node: %p\n", list->next);
+        printf("Previous node: %p\n", list->previous);
+        list = list->next;
+    }
 }
 
 void insertentry(struct entry *newEntry, struct entry *listEntry)
@@ -30,5 +36,27 @@ int main()
 {
     struct entry n1, n2, n3, n4, n5, n6;
     struct entry *head_pointer = &n1;
+
+    n1.next = &n2;
+    n1.previous = NULL;
+    n1.value = 100;
+    n2.next = &n3;
+    n2.previous = &n1;
+    n2.value = 200;
+    n3.next = &n4;
+    n3.previous = &n2;
+    n3.value = 300;
+    n4.next = &n5;
+    n4.previous = &n3;
+    n4.value = 400;
+    n5.next = &n6;
+    n5.previous = &n4;
+    n5.value = 500;
+    n6.next = NULL;
+    n6.previous = &n5;
+    n6.value = 600;
+
+    printlist(head_pointer);
+
     return 0;
 }
