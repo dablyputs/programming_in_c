b58a5d35e6ce62bb7657c3a07f7b61a891caacb9
dablyputs <david.sewell@gmail.com>
2025-01-25 10:36:57 -0500

    Remove prototype from main

--- 10_pointers/10_11_pointer_to_array.c
+++ 10_pointers/10_11_pointer_to_array.c
@@ -17,10 +17,10 @@ int arraySum(int array[], const int n)
 
 int main(int argc, char const *argv[])
 {
-    int arraySum(int array[], const int n);
     int values[10] = { 3, 7, -9, 3, 6, -1, 7, 9, 1, -5};
 
     printf("The sum is %i\n", arraySum(values, 10));
 
     return 0;
+
 }
